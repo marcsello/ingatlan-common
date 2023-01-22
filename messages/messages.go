d60f2a44f447a6461a028fbcd0acdc40a84c8985
marcsello <punkosdmarcell@rocketmail.com>
2023-01-22 20:47:24 +0100

    stuff

---
+++ messages/messages.go
@@ -0,0 +1,7 @@
+package messages
+
+import "github.com/marcsello/ingatlan-common/model"
+
+type AlbiUpdateInfo struct {
+	Albi model.Albi `json:"albi"`
+}
--- vals/redis_keys.go
+++ vals/redis_keys.go
@@ -1,6 +1,9 @@
 package vals
 
 const ( // naming convention roughly: <service><type><name>
-	ScraperRedisKeyLastRun = "SCRAPER:LAST_RUN"
-	JanitorRedisKeyLastRun = "JANITOR:LAST_RUN"
+	ScraperRedisKeyLastRun    = "SCRAPER:LAST_RUN"
+	ScraperRedisKeyInProgress = "SCRAPER:RUNNING"
+
+	JanitorRedisKeyLastRun    = "JANITOR:LAST_RUN"
+	JanitorRedisKeyInProgress = "JANITOR:RUNNING"
 )
