a67668da9684b321d9dd8703cba6d45e02f26c30
marcsello <punkosdmarcell@rocketmail.com>
2023-01-27 22:03:07 +0100

    added thumbnial url validator

---
+++ tools/imgurl.go
@@ -0,0 +1,29 @@
+package tools
+
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
+// IsValidImageUrl does some http requests to check if the url is available, and it resolves to a valid image by checking the mime type
+func IsValidImageUrl(url string) bool {
+
+	if url == "" {
+		return false
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return false
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+
+	mimeType := http.DetectContentType(body)
+	mimeLeft := strings.Split(mimeType, "/")[0]
+	return mimeLeft == "image"
+}
